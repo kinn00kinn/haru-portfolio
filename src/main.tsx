d4d88a36d609500be7e4e59b4c0a34da829b4aff
kinn00kinn <haruki1009kk@gmail.com>
2025-11-17 21:37:34 +0900

    静的サイトで動くように修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { HashRouter } from "react-router-dom"; // 1. HashRouter をインポート
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* 2. <App /> を <HashRouter> で囲む */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
